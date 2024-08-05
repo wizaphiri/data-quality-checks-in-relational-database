175836583e3b5119e463f7fc8e4d585e9cf1cd27
Wiza <64467592+wizaphiri@users.noreply.github.com>
2024-08-05 12:36:23 +0200

    Update data_consistency_checks.py

--- data_consistency_checks.py
+++ data_consistency_checks.py
@@ -1,92 +1,134 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine
-from datetime import datetime
+from sqlalchemy import create_engine, text
+import os
 import mysql.connector
 import pandas as pd
-import os
+from datetime import datetime
+from dotenv import load_dotenv
+from pandas.io.sql import DatabaseError
 
 import warnings
 warnings.filterwarnings('ignore')
 
-# check missingness and timeliness of individual schemas #obs #encounters #orders
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': openmrs schema checks started')
+# define custom exception
+class TableNotFoundError(Exception):
+    def __init__(self, database_name, table_name):
+        super().__init__(f"Table '{table_name}' not found in database '{database_name}'.")
+        self.database_name = database_name
+        self.table_name = table_name
 
-def connect_to_database(hostname, username, password, database):
-    connection = mysql.connector.connect(
+def connect_to_database(hostname, username, password, database, port=3306):
+    return mysql.connector.connect(
         host=hostname,
         user=username,
         password=password,
         database=database,
+        port=port
     )
-    return connection
 
-max_fetch_value, hostname, ctap, db_maxConnTime, password, username = "0056", "127.0.0.1", "xtp-501", 6000, "test", "test"  
+load_dotenv()
+hostname = os.getenv('DB_HOSTNAME')
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
 
-connection = connect_to_database(hostname, username, password, None) 
-cursor = connection.cursor()
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f": scanning openmrs schemas on {hostname} mysql instance")
+try:
+    connection = connect_to_database(hostname, username, password, None)
+    cursor = connection.cursor()
 
-db_list_query = "show databases" 
-databases_df = pd.read_sql_query(db_list_query,connection) 
-rds_databases_df = databases_df[databases_df['Database'].str.startswith('openmrs_')] 
+    db_list_query = "SHOW DATABASES"
+    databases_df = pd.read_sql_query(db_list_query, connection)
+    rds_databases_df = databases_df[databases_df['Database'].str.startswith('openmrs_')]
 
-drop_lst = ['openmrs_','openmrs_area18_test','openmrs_khonjeni_Thu','openmrs_mulanje_dh_Thu','openmrs_devops']
-rds_databases_df = rds_databases_df[~rds_databases_df['Database'].isin(drop_lst)]
+    loading_status_query = """
+        SELECT 
+            (SELECT property_value FROM global_property gp WHERE property = 'current_health_center_id') AS facility_id,
+            (SELECT DISTINCT name FROM location l WHERE location_id = (SELECT DISTINCT property_value FROM global_property WHERE property='current_health_center_id')) AS facility_name,
+            'obs' AS table_name,
+            COALESCE(COUNT(*), 0) AS record_count,
+            MAX(DATE(obs_datetime)) AS max_date,
+            QUARTER(NOW())-1 AS reporting_quarter 
+        FROM obs o 
+        WHERE obs_datetime < NOW()
+        
+        UNION ALL 
+        
+        SELECT 
+            (SELECT property_value FROM global_property gp WHERE property = 'current_health_center_id') AS facility_id,
+            (SELECT DISTINCT name FROM location l WHERE location_id = (SELECT DISTINCT property_value FROM global_property WHERE property='current_health_center_id')) AS facility_name,
+            'encounter' AS table_name,
+            COALESCE(COUNT(*), 0) AS record_count,
+            MAX(DATE(encounter_datetime)) AS max_date,
+            QUARTER(NOW())-1 AS reporting_quarter 
+        FROM encounter e 
+        WHERE encounter_datetime < NOW()
+        
+        UNION ALL 
+        
+        SELECT 
+            (SELECT property_value FROM global_property gp WHERE property = 'current_health_center_id') AS facility_id,
+            (SELECT DISTINCT name FROM location l WHERE location_id = (SELECT DISTINCT property_value FROM global_property WHERE property='current_health_center_id')) AS facility_name,
+            'orders' AS table_name,
+            COALESCE(COUNT(*), 0) AS record_count,
+            MAX(DATE(start_date)) AS max_date,
+            QUARTER(NOW())-1 AS reporting_quarter 
+        FROM orders r
+        WHERE start_date < NOW()
+    """
 
-loading_status_query = """
-                        select 
-                            (select property_value from global_property gp where property = 'current_health_center_id') as facility_id,
-                            (select distinct name from location l where location_id = (select distinct property_value from global_property where property='current_health_center_id')) facility_name,
-                            'obs' table_name,
-                            coalesce(count(*),0) record_count,
-                            max(date(obs_datetime)) max_date,
-                            quarter(now())-1 reporting_quarter 
-                            from obs o 
-                            where obs_datetime < now()                      
-                            union all
-                            select 
-                            (select property_value from global_property gp where property = 'current_health_center_id') as site_id,
-                            (select distinct name from location l where location_id = (select distinct property_value from global_property where property='current_health_center_id')) facility_name,
-                            'encounter' table_name,
-                            coalesce(count(*),0) record_count,
-                            max(date(encounter_datetime)) max_date,
-                            quarter(now())-1 reporting_quarter 
-                            from encounter e 
-                            where encounter_datetime < now()
-                            union all 
-                            select 
-                            (select property_value from global_property gp where property = 'current_health_center_id') as site_id,
-                            (select distinct name from location l where location_id = (select distinct property_value from global_property where property='current_health_center_id')) facility_name,
-                            'orders' table_name,
-                            coalesce(count(*),0) record_count,
-                            max(date(start_date)) max_date,
-                            quarter(now())-1 reporting_quarter 
-                            from orders r
-                     where start_date < now()
-                            """
-column_names = ['facility_id','facility_name','table_name','record_count','max_date','reporting_quarter']
-loading_status_df = pd.DataFrame(columns=column_names)
+    column_names = ['facility_id', 'facility_name', 'table_name', 'record_count', 'max_date', 'reporting_quarter']
+    loading_status_df = pd.DataFrame(columns=column_names)
 
-xcountr = int(rds_databases_df.shape[0])
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+':',str(xcountr)+' openmrs schemas selected for processing')
+    xcountr = int(rds_databases_df.shape[0])
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ':', str(xcountr) + ' openmrs schemas selected for processing')
 
-for index, database in rds_databases_df.iterrows():
-    database_name = database[0]
-    connection = connect_to_database(hostname, username, password, database_name)
-    loading_status = pd.read_sql_query(loading_status_query,connection) 
-    loading_status_df = loading_status.append(loading_status_df, ignore_index=True)  
+    for index, database in rds_databases_df.iterrows():
+        database_name = database['Database']
+        connection = None
+        try:
+            connection = connect_to_database(hostname, username, password, database_name)
+            try:
+                loading_status = pd.read_sql_query(loading_status_query, connection)
+                loading_status_df = pd.concat([loading_status_df, loading_status], ignore_index=True)
+            except DatabaseError as err:
+                if "1146 (42S02)" in str(err):
+                    # Check which table is missing by inspecting the SQL query and error message
+                    if 'obs' in str(err):
+                        missing_table = 'obs'
+                    elif 'encounter' in str(err):
+                        missing_table = 'encounter'
+                    elif 'orders' in str(err):
+                        missing_table = 'orders'
+                    else:
+                        missing_table = 'unknown'
 
-connection.close()
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Skipping database '{database_name}' - Table '{missing_table}' does not exist.")
+                else:
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Error querying database '{database_name}' - {err}")
+                    raise  # Re-raise any other database-related errors
 
-dqa_df =  pd.DataFrame(loading_status_df, columns = ['facility_id','facility_name','table_name', 'max_date'])
+        except mysql.connector.Error as err:
+            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Error connecting to database '{database_name}' - {err}")
+            raise  # Re-raise connection errors
+
+        finally:
+            if connection:
+                connection.close()
+
+    schema_stat = str(loading_status_df.shape)
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ': dataframe shape ' + schema_stat)
+
+except mysql.connector.Error as err:
+    print(f"Failed to connect to MySQL server: {err}")
+
+dqa_df = pd.DataFrame(loading_status_df, columns=['facility_id', 'facility_name', 'table_name', 'max_date'])
 pivot_df = dqa_df.pivot_table(index=['facility_id', 'facility_name'], columns='table_name', values='max_date', aggfunc='max')
+pivot_df = pivot_df.reset_index()
 
-# dqa_df.columns = [col for col in pivot_df.columns] 
-dqa_df = dqa_df.reset_index()
-
+# rename columns to add '_max_date' suffix
 new_column_names = {col: col + '_max_date' if col not in ['facility_id', 'facility_name'] else col for col in pivot_df.columns}
 pivot_df.rename(columns=new_column_names, inplace=True)
 
-# std dev
 pivot_df['encounter_max_datex'] = pd.to_datetime(pivot_df['encounter_max_date'])
 pivot_df['obs_max_datex'] = pd.to_datetime(pivot_df['obs_max_date'])
 pivot_df['orders_max_datex'] = pd.to_datetime(pivot_df['orders_max_date'])
@@ -96,19 +138,33 @@ pivot_df['date2_ordinal'] = pivot_df['obs_max_datex'].map(pd.Timestamp.toordinal
 pivot_df['date3_ordinal'] = pivot_df['orders_max_datex'].map(pd.Timestamp.toordinal)
 
 pivot_df['std_dev'] = pivot_df[['date1_ordinal', 'date2_ordinal', 'date3_ordinal']].std(axis=1)
-pivot_df['std_dev'] = pivot_df['std_dev'].round(2)
+pivot_df['std_dev'] = pivot_df['std_dev'].round()
 
 pivot_df = pivot_df.reset_index()
 pivot_df = pivot_df[['facility_id', 'facility_name', 'encounter_max_date', 'obs_max_date', 'orders_max_date', 'std_dev']]
 
 ycountr = int(pivot_df.shape[0])
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+':',str(ycountr)+' out of',str(xcountr),'openmrs schemas processed succesfully')
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ':', str(ycountr) + ' out of', str(xcountr), 'openmrs schemas processed successfully')
 
-engine = create_engine('mysql+pymysql://test:test@127.0.0.1/temp')
+pivot_df['date_created'] = datetime.now().date()
+
+load_dotenv()
+
+db_type = os.getenv('DB_TYPE')
+user = os.getenv('DB_USER')
+port = os.getenv('DB_PORT')
+host = os.getenv('DB_HOSTNAME')
+database = os.getenv('DB_DATABASE')
+token = os.getenv('DB_TOKEN')
+
+connection_string = f"{db_type}://{user}:{token}@{host}:{port}/{database}"
+engine = create_engine(connection_string)
+
 with engine.begin() as conn:
-    conn.exec_driver_sql("DROP TABLE IF EXISTS temp_dqa_openmrs_schemas")
-    pivot_df.to_sql('temp_dqa_openmrs_schemas', engine, if_exists='append')
-
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': temp_dqa_openmrs_schemas table populated')
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': openmrs schema checks complete') 
-
+    conn.execute("DROP TABLE IF EXISTS temp_dqa_openmrs_schemas")
+    pivot_df.to_sql('temp_dqa_openmrs_schemas', engine, if_exists='replace', index=False)
+    result = conn.execute(text("SELECT COUNT(*) FROM temp_dqa_openmrs_schemas"))
+    record_count = result.scalar()
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {record_count} records inserted into 'temp_dqa_openmrs_schemas'.")
+    
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ': openmrs schema analysis complete')
